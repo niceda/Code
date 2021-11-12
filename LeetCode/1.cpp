d7450d7a533709cdead2f3a1389d0801b5eef950
fida <iwanderuu@gmail.com>
2021-11-13 07:58:10 +0800

    update

--- LeetCode/1.cpp
+++ LeetCode/1.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-      int p1, p2;
-      for(int i = 0; i < nums.size(); i++) {
-        for(int j = i + 1; j < nums.size(); j++) {
-          if(nums[i] + nums[j] == target)
-            p1 = i; p2 = j;
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int p1, p2;
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = i + 1; j < nums.size(); j++) {
+        if (nums[i] + nums[j] == target)
+          p1 = i;
+        p2 = j;
       }
     }
-    return vector<int>{p1 , p2};
-    }
+    return vector<int>{p1, p2};
+  }
 };
